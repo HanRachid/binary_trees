332718fa9d7e1c53bd9d76a83615269b36c1612e
HanRachid <handaouirachid@gmail.com>
2023-10-31 11:18:25 +0100

    edit README.md

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * binary_tree_insert_left - insert a node in left
@@ -9,6 +8,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+
 	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	new_node->parent = parent;
